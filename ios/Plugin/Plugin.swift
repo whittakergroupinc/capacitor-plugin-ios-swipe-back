6a80abc35800d2187905e1b3eddbae9d1ac7e4ea
David <david@whittakergroupinc.com>
2022-03-07 09:09:28 +0100

    initial commit

---
+++ ios/Plugin/Plugin.swift
@@ -0,0 +1,24 @@
+import Foundation
+import Capacitor
+
+/**
+ * Please read the Capacitor iOS Plugin Development Guide
+ * here: https://capacitor.ionicframework.com/docs/plugins/ios
+ */
+@objc(IosSwipeBack)
+public class IosSwipeBack: CAPPlugin {
+    
+    @objc func enable(_ call: CAPPluginCall) {
+        self.bridge.getWebView()?.allowsBackForwardNavigationGestures = true;
+        call.resolve([
+            "error": 0
+            ]);
+    }
+    
+    @objc func disable(_ call: CAPPluginCall) {
+        self.bridge.getWebView()?.allowsBackForwardNavigationGestures = false;
+        call.resolve([
+            "error": 0
+            ]);
+    }
+}
